9fd263d4637381d99891310f7c8042b9b4d5c50a
exception-raised <dimmelman@riseup.net>
2023-12-04 14:05:06 +0200

    Day 1 complete, works for both stars

--- 2023/Day 1/main.py
+++ 2023/Day 1/main.py
@@ -2,25 +2,34 @@ import re
 
 input_file = open("Day 1/input.txt")
 
+mapped = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+def get_data_from_string(word):
+    matches = re.findall(r'(?=(0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine))', word)
+    if len(matches) != 0:
+        return matches
+    return []
+
+
+def get_digit_from_word(word, position):
+    value = get_data_from_string(word)[position]
+    return int(mapped.get(value, value)) 
+
 total = 0
-while(True):
-    line = input_file.readline()
-    if(not line):
-        break
+for line in input_file:
+    left    =  get_digit_from_word(line, 0)
+    right   =  get_digit_from_word(line, -1)
+    total += left*10 + right # Example: 8, 5 ; 8 * 10 = 80 ; 80 + 5 = 85
 
-    left_pointer = -1
-    right_pointer = -1
-
-    
-    for i in range(len(line)):
-        if(line[i].isdigit()):
-            if(left_pointer == -1):
-                left_pointer = int(line[i])
-                right_pointer = int(line[i])
-            else:
-                right_pointer = int(line[i])
-
-    total += left_pointer*10 + right_pointer # Example: 8, 5 ; 8 * 10 = 80 ; 80 + 5 = 85
-
-print(total)
+print(f"Answer: {total}")
 input_file.close()
